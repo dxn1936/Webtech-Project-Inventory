9afc39fe665ac72376c18da3b6d1f5ade3999e15
dxn1936 <mca.1936@unigoa.ac.in>
2021-05-26 23:39:05 +0530

    commit export

---
+++ src/inventory/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import * 
+
+# Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Warehouse)
+admin.site.register(Products)
+admin.site.register(Suppliers)
+admin.site.register(Product_items_details)
